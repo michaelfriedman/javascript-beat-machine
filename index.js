85b1711348ed59919b8dfd215b2bf3dc8a4f234e
Michael Friedman <contact@michaeldavidfriedman.com>
2017-04-19 08:10:38 -0700

    JavaScript beat maching functioning successfully

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 (function () {
   'use strict';
 
-  window.addEventListener('keydown', (event) => {
+  function playSound (event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
-    console.log('event', key);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-  });
+    key.classList.add('playing');
+  }
+
+  function removeTransition (event) {
+    if (event.propertyName !== 'transform') return;
+    event.target.classList.remove('playing');
+  }
+
+  const keys = document.querySelectorAll('.key');
+  keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
 }());
